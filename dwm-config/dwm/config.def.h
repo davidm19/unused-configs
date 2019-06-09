7e256d6e68caf5e26c86d626f5e5ffa6e975ecab
davidm19 <dmalone@chadwickschool.org>
2019-06-09 10:25:27 -0700

    Add new font suggestion in dwm

--- dwm-config/dwm/config.def.h
+++ dwm-config/dwm/config.def.h
@@ -7,7 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 /* static const unsigned int startborders = 3; */
-static const char *fonts[]          = { "curie:size=12" };
+static const char *fonts[]          = { "curie:size=12" }; /* FURA CODE */
 static const char dmenufont[]       = "curie:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
