559315dab083efc2aa192ab236a3bec680874bda
davidm19 <dmalone@chadwickschool.org>
2019-06-18 20:29:28 -0700

    Add documentation to xmobar config file

--- xmonad/xmobar.hs
+++ xmonad/xmobar.hs
@@ -1,6 +1,15 @@
+{-|
+
+   File: xmobar.hs
+   Description: Short and sweet xmobar configuration (which is still in its infancy)
+   Author: davidm19
+   Date: June 18th, 2019
+
+-}
+
 Config { 
 
-   -- appearance
+   -- Section: Appearance
      font =         "xft:Bitstream Vera Sans Mono:size=9:bold:antialias=true"
    , bgColor =      "black"
    , fgColor =      "#cccccc"
@@ -8,35 +17,21 @@ Config {
    , border =       BottomB
    , borderColor =  "#646464"
 
-   -- layout
-   , sepChar =  "%"   -- delineator between plugin names and straight text
-   , alignSep = "}{"  -- separator between left-right alignment
+   -- Section: Layout
+   , sepChar =  "%"
+   , alignSep = "}{"
    , template = "%StdinReader% | %multicpu% | %coretemp% | %memory% | %dynnetwork% }{ %date% || %kbd% "
 
-   -- general behavior
-   , lowerOnStart =     True    -- send to bottom of window stack on start
-   , hideOnStart =      False   -- start with window unmapped (hidden)
-   , allDesktops =      True    -- show on all desktops
-   , overrideRedirect = True    -- set the Override Redirect flag (Xlib)
-   , pickBroadest =     False   -- choose widest display (multi-monitor)
-   , persistent =       True    -- enable/disable hiding (True = disabled)
+   -- Section: general behavior
+   , lowerOnStart =     True
+   , hideOnStart =      False
+   , allDesktops =      True
+   , overrideRedirect = True
+   , pickBroadest =     False
+   , persistent =       True
 
-   -- plugins
-   --   Numbers can be automatically colored according to their value. xmobar
-   --   decides color based on a three-tier/two-cutoff system, controlled by
-   --   command options:
-   --     --Low sets the low cutoff
-   --     --High sets the high cutoff
-   --
-   --     --low sets the color below --Low cutoff
-   --     --normal sets the color between --Low and --High cutoffs
-   --     --High sets the color above --High cutoff
-   --
-   --   The --template option controls how the plugin is displayed. Text
-   --   color can be set by enclosing in <fc></fc> tags. For more details
-   --   see http://projects.haskell.org/xmobar/#system-monitor-plugins.
+   -- Scetion: Plugins
    , commands = 
-
         -- network activity monitor (dynamic interface resolution)
         [ Run DynNetwork     [ "--template" , "<dev>: <tx>kB/s|<rx>kB/s"
                              , "--Low"      , "1000"       -- units: B/s
