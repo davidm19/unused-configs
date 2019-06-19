1dcf213dd76f77a98f0c7113151d1651f4066219
davidm19 <dmalone@chadwickschool.org>
2019-06-18 20:02:35 -0700

    Add initial keybindings and float, monocle layouts

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,14 @@
 import XMonad
+import System.Exit (exitSuccess)
 import XMonad.Config.Desktop
 import XMonad.Layout.Dwindle
+import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Hooks.DynamicLog
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
+import XMonad.Util.EZConfig (additionalKeysP)
+import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
 
 -- Main Function
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
@@ -10,7 +17,7 @@ main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 myConfig = desktopConfig
      { layoutHook = myLayout
      , terminal   = "st"
-     }
+     } `additionalKeysP`         myKeys
 
 -- XMobar Configuration
 myBar = "xmobar"
@@ -18,7 +25,23 @@ myPP  = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 -- Layouts
-myLayout = Dwindle R CW (3/2) (11/10)
+myLayout = smartBorders
+         $ mkToggle (NOBORDERS ?? FULL ?? EOT)
+         $ Dwindle R CW (3/2) (11/10) ||| simplestFloat ||| Full
+
+-- Keybindings
+myKeys =
+        -- Xmonad
+        [ ("M-C-r", spawn "xmonad --recompile")      -- Recompiles xmonad
+        , ("M-S-r", spawn "xmonad --restart")        -- Restarts xmonad
+        , ("M-S-q", io exitSuccess)                  -- Quits xmonad
+
+        -- Layouts
+        , ("M-<Space>", sendMessage NextLayout)      -- Switch to next layout
+        , ("M-S-f", sendMessage (T.Toggle "simpleFloat"))
+        , ("M-S-m", sendMessage $ Toggle FULL ) ]
+
+
 -- namedScratchpads (dwt1)
 
 -- simpleFloat
