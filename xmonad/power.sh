3668f7f668743c2152956989f81fd8e5cb72e38b
davidm19 <dmalone@chadwickschool.org>
2019-06-20 16:19:32 -0700

    Add power management script

---
+++ xmonad/power.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Adapted from this gist: https://gist.github.com/adnan360/f86012baeb4c9ca4f1af033550b03033  
+
+chosen=$(echo -e "Cancel\nSuspend\nReboot\nShutdown\nHibernate" | rofi -dmenu -i)
+
+if [[ $chosen = "Suspend" ]]; then
+	systemctl suspend
+elif [[ $chosen = "Reboot" ]]; then
+	systemctl reboot
+elif [[ $chosen = "Shutdown" ]]; then
+	systemctl poweroff
+elif [[ $chosen = "Hibernate" ]]; then
+	systemctl hibernate
+fi
