7d5ead7693402cbacdfea3210336ec2bb6ba3007
davidm19 <dmalone@chadwickschool.org>
2019-06-20 16:18:57 -0700

    Add bootstrapping script outline for xmonad

---
+++ xmonad/bootstrap.sh
@@ -0,0 +1,10 @@
+# steps:
+# 1. determine OS
+# 2. install rofi, feh, st, + other dependencies
+# 3. install cabal
+# 4. install xmonad, contrib, and xmobar
+# 5. install fonts (curie, iosevka, terminus)
+# 6. move and load Xresources + oxide theme
+# 7. move and load xmonad, xmobar configs
+# 8. move and rename power script
+# 9. move xinitrc
