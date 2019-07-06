4753dc1af971014a8380df17739e150d3d0e3ffd
davidm19 <dmalone@chadwickschool.org>
2019-07-05 22:04:06 -0700

    Add separation between tabs/non-tabbed layout hooks
    Also do you really need title in xmobar?

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -47,7 +47,9 @@ main = do
          , terminal   = myTerminal
          , logHook    = dynamicLogWithPP $ defaultPP
              { ppOutput  = hPutStrLn xmproc
-	     , ppTitle   = xmobarColor "#ff00ff" "" . shorten 50
+	     , ppTitle   = xmobarColor "#ff00ff" "" . shorten 50 -- but to be honest do you need a
+	                                                         -- title? i mean, you already know
+								 -- what program you're in...
 	     , ppLayout  = xmobarColor "#ffff00" ""
 	     , ppSep     = "<fc=#b2b2b2> :: </fc>"
 	     , ppCurrent = xmobarColor "#00ffff" "" . wrap "[" "]"
@@ -63,13 +65,12 @@ main = do
 myTerminal = "st"
 
 -- Section: Layouts
--- myLayout = smartBorders $ avoidStruts $ mkToggle (NOBORDERS ?? FULL ?? EOT)
---          $ ResizableTall 1 (3/100) (1/2) [] ||| TwoPane (3/100) (1/2) ||| simplestFloat ||| noBorders (tabbed shrinkText myTabConfig)
-
--- myLayout = smartBorders $ avoidStruts $ gaps [(U,5), (D,5), (L,12), (R,12)] $ spacingRaw True (Border 0 2 2 2) True (Border 0 2 2 2) True $ mkToggle (NOBORDERS ?? FULL ?? EOT) $ myDefaultLayout
 myLayout = smartBorders $ avoidStruts $ mkToggle (NOBORDERS ?? FULL ?? EOT) $ myDefaultLayout
          where
 	     myDefaultLayout = tall ||| twoPane ||| floater ||| tab
+
+-- GAPS!
+-- myLayout = smartBorders $ avoidStruts $ gaps [(U,5), (D,5), (L,12), (R,12)] $ spacingRaw True (Border 0 2 2 2) True (Border 0 2 2 2) True $ mkToggle (NOBORDERS ?? FULL ?? EOT) $ myDefaultLayout
 
 tall    = renamed [Replace "Tall"] $ ResizableTall 1 (3/100) (1/2) []
 twoPane = renamed [Replace "Two Pane"] $ TwoPane (3/100) (1/2)
