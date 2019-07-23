ebd2363cbdbc5f7c94fa2134942b965fc2385a1a
davidm19 <dmalone@chadwickschool.org>
2019-07-22 22:00:50 -0700

    Update instructions for bootstrap script

--- xmonad/bootstrap.sh
+++ xmonad/bootstrap.sh
@@ -1,9 +1,10 @@
 # steps:
 # 1. determine OS
-# 2. install rofi, feh, sflock, st, + other dependencies
+# 2. install rofi, feh, sflock + other dependencies
 # 3. install xmonad, contrib, and xmobar
 # 4. install fonts (curie, Inconsolata, terminus)
-# 5. move and load Xresources + oxide theme
-# 6. move and load xmonad, xmobar configs
-# 7. move and rename power script
-# 8. move xinitrc
+# 5. build st
+# 6. move and load Xresources
+# 7. move and load xmonad, xmobar configs
+# 8. move and rename power script
+# 9. move xinitrc
