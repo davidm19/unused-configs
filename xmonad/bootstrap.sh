a504a31a629f092e0184962022321636ae06f7cd
davidm19 <dmalone@chadwickschool.org>
2019-07-22 18:29:00 -0700

    Remove cabal as bootstrap step as it doesn't freaking work

--- xmonad/bootstrap.sh
+++ xmonad/bootstrap.sh
@@ -1,10 +1,9 @@
 # steps:
 # 1. determine OS
 # 2. install rofi, feh, sflock, st, + other dependencies
-# 3. install cabal and cabal update
-# 4. install xmonad, contrib, and xmobar
-# 5. install fonts (curie, Inconsolata, terminus)
-# 6. move and load Xresources + oxide theme
-# 7. move and load xmonad, xmobar configs
-# 8. move and rename power script
-# 9. move xinitrc
+# 3. install xmonad, contrib, and xmobar
+# 4. install fonts (curie, Inconsolata, terminus)
+# 5. move and load Xresources + oxide theme
+# 6. move and load xmonad, xmobar configs
+# 7. move and rename power script
+# 8. move xinitrc
